c10599f20f5975f9b8a67fba30ad278ec7f0a7fc
Gabriel Anguiano <anguianog97@gmail.com>
2019-09-30 12:49:24 -0700

    main section done

--- js/index.js
+++ js/index.js
@@ -40,3 +40,57 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+// anchor Tags
+let aLinks = document.getElementsByTagName('a');
+console.log(aLinks); 
+aLinks[0].textContent = siteContent['nav']['nav-item-1']; 
+aLinks[1].textContent = siteContent['nav']['nav-item-2']; 
+aLinks[2].textContent = siteContent['nav']['nav-item-3']; 
+aLinks[3].textContent = siteContent['nav']['nav-item-4']; 
+aLinks[4].textContent = siteContent['nav']['nav-item-5']; 
+aLinks[5].textContent = siteContent['nav']['nav-item-6']; 
+
+let mainHeader = document.querySelector('h1');
+mainHeader.textContent = siteContent['cta']['h1'];
+
+let button = document.querySelector('button');
+button.textContent = siteContent['cta']['button'];
+
+let headingImg = document.getElementById('cta-img');
+headingImg.setAttribute('src', siteContent['cta']['img-src']);
+
+
+//aLinks[0].textContent = 'Services'; 
+// Array.from(aLinks)
+//  const anchorText = Array.from(aLinks).forEach((link,text) =>{
+//    link.textContent = text; 
+//  })
+
+
+
+// top Content 
+// headers
+let subHeader = document.querySelectorAll('h4');
+subHeader[0].textContent = siteContent['main-content']['features-h4'];
+subHeader[1].textContent = siteContent['main-content']['about-h4'];
+subHeader[2].textContent = siteContent['main-content']['services-h4'];
+subHeader[3].textContent = siteContent['main-content']['product-h4'];
+subHeader[4].textContent = siteContent['main-content']['vision-h4'];
+
+// main-section-img
+
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+ //paragraphs 
+
+ let mainSectionParagraphs = document.querySelectorAll('p');
+const paragraphSection = (value,text) => {
+  return value.textContent = text;
+}
+paragraphSection(mainSectionParagraphs[0],siteContent['main-content']['features-content']);
+paragraphSection(mainSectionParagraphs[1],siteContent['main-content']['about-content']);
+paragraphSection(mainSectionParagraphs[2],siteContent['main-content']['services-content']);
+paragraphSection(mainSectionParagraphs[3],siteContent['main-content']['product-content']);
+paragraphSection(mainSectionParagraphs[4],siteContent['main-content']['vision-content']);
+
